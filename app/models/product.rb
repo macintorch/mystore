d255743fb683b4794724e7f16a8ac85e5e03d55c
macintorch <macintorch@gmail.com>
2016-09-08 11:54:12 +0800

    create admin section

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 
 	validates :title, :description, presence: true
-	validates :price, :numericality: {greater_than_or_equal_to: 0.01}
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
